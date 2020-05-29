5d3392b01a41d849da24359ba4e2127717a7118d
SVKratzer <samuel.kratzer1894@gmail.com>
2020-05-29 13:58:17 -0400

    Implement bfs and dfs instance methods into PolyTreeNode class

--- W3D5/polytree_project/lib/00_tree_node.rb
+++ W3D5/polytree_project/lib/00_tree_node.rb
@@ -1,6 +1,5 @@
 class PolyTreeNode
-    attr_reader :children, :value
-
+    attr_reader :children, :value, :parent
 
     def initialize(value)
         @value = value
@@ -13,9 +12,43 @@ class PolyTreeNode
             @parent = nil
             return
         end
+        self.parent.children.delete(self) unless @parent.nil?
         @parent = node
-        node.children << self
+        node.children << self if !node.children.include?(self)
+
     end
-    
+
+    def add_child(child_node)
+      child_node.parent = self
+    end
+
+    def remove_child(child_node)
+      if @children.include?(child_node)
+        child_node.parent = nil
+      else
+        raise "CHILD NOT FOUND"
+      end
+    end
+
+    def dfs(target)
+      return self if self.value == target 
+      return nil if self.value.nil?
+      @children.each do |child|
+        result = child.dfs(target)
+        return result unless result.nil?
+      end
+      nil
+    end
+
+    def bfs(target)
+      queue = [self]
+      until queue.empty?
+        curr_node = queue.shift
+        return curr_node if curr_node.value == target
+        queue += curr_node.children
+      end
+      nil
+    end
 
 end
+
