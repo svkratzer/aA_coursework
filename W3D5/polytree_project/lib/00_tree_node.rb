ce3186051677c994a19f1e98bf62ff1fdb17e795
SVKratzer <samuel.kratzer1894@gmail.com>
2020-05-29 12:18:39 -0400

    Commit gemfile.lock and begin defining PolyTreeNode methods

--- W3D5/polytree_project/lib/00_tree_node.rb
+++ W3D5/polytree_project/lib/00_tree_node.rb
@@ -1,4 +1,21 @@
 class PolyTreeNode
+    attr_reader :children, :value
+
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(node)
+        if node == nil
+            @parent = nil
+            return
+        end
+        @parent = node
+        node.children << self
+    end
     
 
 end
