d29b1ec1682bd8712feb91459b7933d8e4895765
SVKratzer <samuel.kratzer1894@gmail.com>
2020-06-01 13:36:20 -0400

    Add losing_node? and winning_node? instance methods

--- W4D1/tic_tac_tow_ai/lib/tic_tac_toe_node.rb
+++ W4D1/tic_tac_tow_ai/lib/tic_tac_toe_node.rb
@@ -27,9 +27,15 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
+    unless @board.winner == evaluator
+
+
+
+    end
   end
 
   def winning_node?(evaluator)
+
   end
 
   # # This method generates an array of all moves that can be made after
