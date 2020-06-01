93ed02d6c21c4cf5dc82c6c7df6d81269b9e8952
SVKratzer <samuel.kratzer1894@gmail.com>
2020-06-01 12:15:03 -0400

    Add method in TicTacToeNode class that generates children for a given node

--- W4D1/tic_tac_tow_ai/lib/tic_tac_toe_node.rb
+++ W4D1/tic_tac_tow_ai/lib/tic_tac_toe_node.rb
@@ -1,16 +1,29 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board = board
     @next_mover_mark = next_mover_mark
     @prev_move_pos = prev_move_pos
   end
 
-  def children
-    @board.roeach do |row|
 
+  
+  def children
+    children = []
+    @board.rows.each_with_index do |row, i|
+      row.each_with_index do |pos, j|
+        if pos.nil?
+          new_board = @board.dup
+          new_board.rows[i][j] = @next_mover_mark
+          mover_mark = @next_mover_mark == :x ? :o : :x
+          children << TicTacToeNode.new(new_board, mover_mark, [i, j])
+        end
+      end
     end
+    children
   end
 
   def losing_node?(evaluator)
@@ -19,9 +32,9 @@ class TicTacToeNode
   def winning_node?(evaluator)
   end
 
-  # This method generates an array of all moves that can be made after
-  # the current move.
-  def children
-  end
+  # # This method generates an array of all moves that can be made after
+  # # the current move.
+  # def children
+  # end
 end
 
