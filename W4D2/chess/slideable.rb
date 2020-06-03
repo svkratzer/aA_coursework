831f50352f08e5aa06cc953b1530121c6dec394f
SVKratzer <samuel.kratzer1894@gmail.com>
2020-06-03 14:35:20 -0400

    Implement pseudocode for horizontal_dirs method

--- W4D2/chess/slideable.rb
+++ W4D2/chess/slideable.rb
@@ -1,3 +1,5 @@
+require_relative "board"
+require 'byebug'
 module Slideable
 
   # [delta_row, delta_col]
@@ -11,7 +13,7 @@ module Slideable
   [1, 0]  # down
 ].freeze
 
- VERTICAL_STEPS = [
+ DIAGONAL_STEPS = [
   [-1, -1], #up + left
   [-1, 1], #up + right
   [1, -1], #down + left
@@ -31,14 +33,62 @@ module Slideable
     end    
 
   end  
+  
+  #return an array of all possible horizontal moves from a given start position
+  def horizontal_dirs(start_pos)
+    moves = [start_pos]
+    debugger
+    HORIZONTAL_STEPS.each do |step|
+      move = start_pos.dup
+      until moves.last.any? {|index| !index.between?(0,7) }
+        new_move = moves.last.dup
+        new_move[0] += step[0]
+        new_move[1] += step[1]
+        moves << new_move
+      end
+      # until move.any? {|index| !index.between?(0,7) }
+      
+      # end
+    end
+    moves
+  end 
+        #tbd on side logic (opponent vs ours) -------------------
+        # if board[move].is_a?(Piece) && piece == opponent_piece
+        #   moves << move
+        #   break
+        # elsif board[move].is_a?(Piece) && piece != opponent_piece
+        #   move[0] -= step[0]
+        #   move[1] -= step[1]
+        #   moves << move
+        #    break
+        # end  
+   
+      # the following takes a step back, once we've moved off the edge of the board
 
-  def horizontal_dirs
+     
 
+  def diagonal_dirs(start_pos)
+    moves = []
+    #return an array of all possible diagonal moves from a given start position
+    up_left = DIAGONAL_STEPS[0]
+    up_right = DIAGONAL_STEPS[1]
+    down_left = DIAGONAL_STEPS[2]
+    down_right = DIAGONAL_STEPS[3]
   end  
 
 end
 
+class Test
+  include Slideable
+  def initialize; end
+end
 
+
+if __FILE__ == $PROGRAM_NAME
+  test = Test.new 
+  test.horizontal_dirs([3,3])
+end
+# ROOK _ _ _ _ _ QUEEN
 
 
 =begin
