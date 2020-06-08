f8751ddd5a5bfe23611fa7575d64c26e22a9d5a0
SVKratzer <samuel.kratzer1894@gmail.com>
2020-06-08 14:56:05 -0400

    Finish MaxIntSet class

--- W5D1/hash_and_lru/lib/p01_int_set.rb
+++ W5D1/hash_and_lru/lib/p01_int_set.rb
@@ -1,14 +1,24 @@
-class MaxIntSet
-  def initialize(max)
-  end
+require "set"
 
+class MaxIntSet
+  attr_reader :store, :max
+
+  def initialize(max)
+    @store = Array.new(max, false)
+    @max = max
+  end
+  
   def insert(num)
+    raise "Out of bounds" unless num.between?(0, self.max - 1)
+    self.store[num] = true
   end
 
   def remove(num)
+    self.store[num] = false
   end
 
   def include?(num)
+    self.store[num]
   end
 
   private
@@ -75,4 +85,11 @@ class ResizingIntSet
 
   def resize!
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+
+  set = MaxIntSet.new(10)
+  p set.store.length
+  p set.store
 end
