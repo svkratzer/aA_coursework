29a0f0bd5fbb1741cf46b4ab268ef0817518a27f
SVKratzer <samuel.kratzer1894@gmail.com>
2020-06-08 16:11:37 -0400

    Complete tasks in p01_int_set.rb

--- W5D1/hash_and_lru/lib/p01_int_set.rb
+++ W5D1/hash_and_lru/lib/p01_int_set.rb
@@ -37,18 +37,24 @@ class IntSet
   end
 
   def insert(num)
+    i = num % @store.length
+    @store[i] << num unless self.include?(num)
   end
 
   def remove(num)
+    self[num].delete(num) if self.include?(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    i = num % @store.length
+    @store[i]
   end
 
   def num_buckets
@@ -57,7 +63,7 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_accessor :count, :store
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
@@ -65,18 +71,31 @@ class ResizingIntSet
   end
 
   def insert(num)
+    i = num % @store.length
+    unless self.include?(num)
+      @store[i] << num 
+      self.count += 1 
+    end
+    resize! if self.count > num_buckets
   end
 
   def remove(num)
+    if self.include?(num)    
+      self[num].delete(num) 
+      self.count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    i = num % @store.length
+    @store[i]
   end
 
   def num_buckets
@@ -84,12 +103,17 @@ class ResizingIntSet
   end
 
   def resize!
+    existing_eles = self.store.dup.flatten
+    new_length = num_buckets * 2
+    self.store = Array.new(new_length) { Array.new }
+    self.count = 0
+    existing_eles.each { |ele| self.insert(ele) }
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
 
-  set = MaxIntSet.new(10)
-  p set.store.length
+  set = ResizingIntSet.new
+  p set.store.length * 2
   p set.store
 end
