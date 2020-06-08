5839c39a46a7e2d08224fd75161702392ddda046
SVKratzer <samuel.kratzer1894@gmail.com>
2020-06-08 17:32:47 -0400

    Make hashing functions

--- W5D1/hash_and_lru/lib/p02_hashing.rb
+++ W5D1/hash_and_lru/lib/p02_hashing.rb
@@ -4,11 +4,20 @@ end
 
 class Array
   def hash
+    return 0 if self.empty?
+    self.inject { |acc, num| acc ^ num * self.index(num) }
   end
 end
 
 class String
   def hash
+    alpha = [*("a".."z"), *("A".."Z")]
+    hash = alpha.index(self[0])
+    self[1..-1].each_char.with_index do |char, i|
+      idx = alpha.index(char)
+      hash = hash ^ idx * i
+    end
+    hash
   end
 end
 
@@ -16,6 +25,20 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    return 0 if self.empty?
+    alpha = [*("a".."z"), *("A".."Z")]
+    hash = self.keys.first.to_s.hash ** (self.values.first.hash + 1)
+    return hash if self.length == 1
+    self.drop(1).each do |k,v|
+      key = k.to_s
+      hash = hash ^ (key.hash ** (v.hash + 1))
+    end
+    hash
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+
+  p [0,1,2,3,4].hash
+
 end
