f712f95fce4a366655d6dea228b7a95855066115
SVKratzer <samuel.kratzer1894@gmail.com>
2020-06-11 14:41:37 -0400

    Build associations in models: course, enrollment, and user

--- W5D4/associations_exercise/app/models/course.rb
+++ W5D4/associations_exercise/app/models/course.rb
@@ -1,2 +1,26 @@
 class Course < ApplicationRecord
+    belongs_to :intructors,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :User
+
+    has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Enrollment
+
+    has_many :enrolled_students,
+    through: :enrollment,
+    source: :student
+
+    belongs_to :prerequisites,
+    primary_key: :id,
+    foreign_key: :id,
+    class_name: :Course
+
+    has_many :prerequisites,
+    primary_key: :id,
+    foreign_key: :prereq_id,
+    class_name: :Course
+    
 end
--- W5D4/associations_exercise/app/models/user.rb
+++ W5D4/associations_exercise/app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :Enrollment
+  
+  has_many :courses,
+    primary_key: :id,
+    foreign_key: :instructor_id,
+    class_name: :Course 
+    
 end
