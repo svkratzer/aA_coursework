8d26ec3cbd3ba3ca93aa55fb97a99e5410737105
SVKratzer <samuel.kratzer1894@gmail.com>
2020-06-16 14:03:55 -0400

    Create custom attr_accessor method

--- W6D2/so_meta/lib/00_attr_accessor_object.rb
+++ W6D2/so_meta/lib/00_attr_accessor_object.rb
@@ -1,5 +1,14 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+  
+    names.each do |name|
+      define_method(name.to_sym) {
+        self.instance_variable_get(name.to_sym)
+      }
+      define_method((name.to_s + '=').to_sym) {
+        self.instance_variable_set(name.to_sym)
+      }
+    end
+
   end
 end
