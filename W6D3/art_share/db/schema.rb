319edde2407edb39c21fef8737f790f9e805522f
SVKratzer <samuel.kratzer1894@gmail.com>
2020-06-17 17:15:46 -0400

    Create Artwork model and artworks table in schema

--- W6D3/art_share/db/schema.rb
+++ W6D3/art_share/db/schema.rb
@@ -10,14 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_17_190109) do
+ActiveRecord::Schema.define(version: 2020_06_17_205506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "artworks", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "image_url", null: false
+    t.integer "artist_id", null: false
+    t.index ["title", "artist_id"], name: "index_artworks_on_title_and_artist_id"
+  end
+
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
+    t.string "username", null: false
   end
 
 end
